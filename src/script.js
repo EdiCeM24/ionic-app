6a0336d5424400f7cc402fd679b5517d19c6c6b1
Marcus Edidiong Cletus <edi_mars@outlook.com>
2025-01-13 09:55:32 +0100

    Install changes made.

---
+++ src/script.js
@@ -0,0 +1,7 @@
+
+const myBtn = document.getElementById('btn');
+myBtn.addEventListener('click', () => {
+    let clearText = document.getElementById("text");
+    clearText.style.display = 'none';
+    console.log('clicked me!');
+})   
